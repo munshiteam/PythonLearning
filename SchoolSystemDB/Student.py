37de8b26608345cffb89312cc34b25ad6a38ceae
Saimuddin Munshi <saimmunshi@gmail.com>
2017-08-28 12:49:55 -0700

    please work

--- SchoolSystemDB/Student.py
+++
@@ -1,7 +0,0 @@
-class Student():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __repr__(self):
-        return ("{0} is a(n) {1} year old student.".format(self.name, self.age))
